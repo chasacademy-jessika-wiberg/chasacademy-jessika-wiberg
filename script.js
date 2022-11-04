26a4f83c5c197d9dac6e45ae07843d3220709a3f
Jessika <113911039+chasacademy-jessika-wiberg@users.noreply.github.com>
2022-11-04 10:13:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+
+let btn = document.getElementById("btn");
+let output = document.getElementById("outputtext");
+
+let number = Math.floor(Math.random() * 100 + 1)
+
+btn.addEventListener("click", checkNumber);
+    
+function checkNumber(gissabtn){
+
+
+    let input = document.getElementById("userInput").value;
+
+    if (input == number){
+        output.innerHTML = "you guessed right, your number was right!"
+     } 
+     else if (input < number)
+     {
+            output.innerHTML = "You guessed too low"
+    }
+if (input > number){
+
+output.innerHTML = "You guessed too high"
+
+}
+
+};
+
+
+
+let open = document.getElementById("open");
+let modal_container = document.getElementById("modal_container");
+let close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+    console.log('click')
+
+    modal_container.classList.add("show")
+
+} ) ;
+
+close.addEventListener("click", () => {
+    console.log('click')
+
+    modal_container.classList.remove("show")
+
+} ) ;
+
+async function getData() {
+    const data = await fetch('cv.json').then(res => res.json())
+    console.log(data)
+    const ulEducation = document.getElementById('list-education')
+    for (const item of data.education) {
+       createLi(ulEducation, item.name)
+    }
+    const ulWork = document.getElementById('list-work')
+    for (const item of data.work) {
+       createLi(ulWork, item.name)
+    }
+}
+
+
+
+
+function createLi(ul, name) {
+    const li = document.createElement('li')
+    li.innerText = name
+    ul.appendChild(li)
+}
+
+getData()
+
